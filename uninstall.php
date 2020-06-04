f00492fd163f5a9cee0973dfb2c04f0b7881c714
Remco Kersten <kersten.remco@outlook.com>
2020-06-04 11:15:26 +0200

    Removed uninstall options in database

--- uninstall.php
+++ uninstall.php
@@ -12,11 +12,3 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
-
-global $wpdb;
-
-$plugin_options = $wpdb->get_results( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'recman_%'" );
-
-foreach( $plugin_options as $option ) {
-    delete_option( $option->option_name );
-}
